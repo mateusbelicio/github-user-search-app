a994f0f7e3121d6d9880daf8e98bceb6a35fdb19
mateusbelicio <mateusbelicio@hotmail.com>
2023-04-25 15:30:27 -0300

    Feat: Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const userData = {
+  "login": "octocat",
+  "id": 1,
+  "node_id": "MDQ6VXNlcjE=",
+  "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/octocat",
+  "html_url": "https://github.com/octocat",
+  "followers_url": "https://api.github.com/users/octocat/followers",
+  "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+  "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+  "organizations_url": "https://api.github.com/users/octocat/orgs",
+  "repos_url": "https://api.github.com/users/octocat/repos",
+  "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/octocat/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "monalisa octocat",
+  "company": "GitHub",
+  "blog": "https://github.com/blog",
+  "location": "San Francisco",
+  "email": "octocat@github.com",
+  "hireable": false,
+  "bio": "There once was...",
+  "twitter_username": "monatheoctocat",
+  "public_repos": 2,
+  "public_gists": 1,
+  "followers": 20,
+  "following": 0,
+  "created_at": "2008-01-14T04:33:35Z",
+  "updated_at": "2008-01-14T04:33:35Z"
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log(userData.bio);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <header>
+        <h1>Devfinder</h1>
+        <button className="theme-btn">Theme</button>
+      </header>
+
+      <form noValidate className="form" action="#">
+        <label htmlFor="search-input" className="form__label">Search</label>
+        <input type="text" className="form__input" id="search-input" />
+        <button type="submit" className="form__btn">Search</button>
+      </form>
+
+      <section className="info">
+        <div className="info__header">
+          <img className="info__image" src="" alt="User photo" />
+          <div className="flex flex--wrap">
+            <div className="flex flex--col">
+              <h2 className="info__name">{userData.name}</h2>
+              <h3 className="info__username">@{userData.login}</h3>
+            </div>
+            <span className="info__join-date">Joined 25 Jan 2011</span>
+          </div>
+        </div>
+
+        <div className="info__content">
+          <p className="info__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum 
+            eveniet cupiditate culpa ut unde itaque.
+          </p>
+          <ul className="info__data-list">
+            <li className="info__data-item">
+              <h4 className="info__data-name">Repos</h4>
+              <span className="info__data-value">8</span>
+            </li>
+            <li className="info__data-item">
+              <h4 className="info__data-name">Followers</h4>
+              <span className="info__data-value">3983</span>
+            </li>
+            <li className="info__data-item">
+              <h4 className="info__data-name">Following</h4>
+              <span className="info__data-value">9</span>
+            </li>
+          </ul>
+          <ul className="info__about-list">
+            <li className="info__about-item">
+              <span className="info__about-icon">icon</span>
+              <span className="info__about-location">San Francisco</span>
+            </li>
+            <li className="info__about-item">
+              <span className="info__about-icon">icon</span>
+              <span className="info__about-site"><a href="https://github.blog" target="_blank">https://github.blog</a></span>
+            </li>
+            <li className="info__about-item">
+              <span className="info__about-icon">icon</span>
+              <span className="info__about-twitter">Not Available</span>
+            </li>
+            <li className="info__about-item">
+              <span className="info__about-icon">icon</span>
+              <span className="info__about-work">@github</span>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </> 
   )
 }
 
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,68 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        --hsl-primary-400: 212 100% 50%;
+        
+        --hsl-secondary-300: 217 20% 51%;            
+        --hsl-secondary-400: 217 35% 45%;            
+        --hsl-secondary-500: 217 21% 21%;        
+        
+        --hsl-neutral-100: 0 0% 100%;            
+        --hsl-neutral-200: 0 0% 99.6%;            
+        --hsl-neutral-300: 227 100% 98%;            
+        --hsl-neutral-400: 222 41% 20%;            
+        --hsl-neutral-500: 220 40% 13%;
+
+        --hsl-accent-400: 0 91% 62%;
+    }
+
+    :root {        
+        --clr-primary-400: hsl(var(--hsl-primary-400) / 1);
+
+        --clr-secondary-300: hsl(var(--hsl-secondary-300) / 1);
+        --clr-secondary-400: hsl(var(--hsl-secondary-400) / 1);
+        --clr-secondary-500: hsl(var(--hsl-secondary-500) / 1);
+
+        --clr-neutral-100: hsl(var(--hsl-neutral-100) / 1);
+        --clr-neutral-200: hsl(var(--hsl-neutral-200) / 1);
+        --clr-neutral-300: hsl(var(--hsl-neutral-300) / 1);
+        --clr-neutral-400: hsl(var(--hsl-neutral-400) / 1);
+        --clr-neutral-500: hsl(var(--hsl-neutral-500) / 1);
+
+        --clr-accent-400: hsl(var(--hsl-accent-400) / 1);
+    }
+
+    :root {
+        --size-100: 0.5rem;             /* 8px */
+        --size-150: 0.625rem;           /* 10px */
+        --size-200: 0.8125rem;          /* 13px */
+        --size-250: 0.9375rem;          /* 15px */
+        --size-300: 1rem;               /* 16px */
+        --size-350: 1.125rem;           /* 18px */
+        --size-400: 1.25rem;            /* 20px */
+        --size-450: 1.375rem;           /* 22px */
+        --size-500: 1.5rem;             /* 24px */
+        --size-550: 1.625rem;           /* 26px */
+        --size-600: 1.75rem;            /* 28px */
+        --size-650: 1.875rem;           /* 30px */
+        --size-700: 2rem;               /* 32px */
+        --size-750: 2.25rem;            /* 36px */
+        --size-800: 2.5rem;             /* 40px */
+        --size-850: 3rem;               /* 48px */
+        --size-900: 3.5rem;             /* 56px */
+        --size-950: 4rem;               /* 64px */
+    }
+
+    body {
+        font-family: 'Space Mono', monospace;
+        min-height: 100vh;
+    }
+
+    html {
+        overflow-x: hidden;
+        scroll-behavior: smooth;
+    }
+`;
+
+export default GlobalStyle;
